5bfa1467faa514d5c8e673c72295fdb4dbb778de
Alejandra Veronica Terceros Arcani <alita4774@gmail.com>
2025-09-17 18:00:34 -0400

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://alita9202.github.io/ProyectoAngular',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
